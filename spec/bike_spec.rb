025897716846d5a60e99d8a1cc3ed402dd0385e0
Jade Beresford <jade_fletcher@hotmail.co.uk>
2019-11-15 15:53:11 +0000

    add broken method to Bike class

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,7 +1,16 @@
 require "bikes"
 
 describe Bike do
-  it "responds to 'working?' method" do
-    expect(subject).respond_to? (:working?)
+
+  describe "#working?" do
+    it "responds to 'working?' method" do
+      expect(subject).respond_to? (:working?)
+    end
+  end
+
+  describe "#broken" do
+    it "returns true if method called" do
+      expect(subject.broken).to eq true
+    end
   end
 end
